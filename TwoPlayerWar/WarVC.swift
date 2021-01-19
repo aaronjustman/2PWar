c60adfd842554ab4da1c16f88d493bec97a3dcab
Aaron Justman <aaronjustman@divided-sky.home>
2021-01-19 12:58:05 -0800

    Added buttons to play cards and got everything for player 2 flipped.

--- TwoPlayerWar/WarVC.swift
+++ TwoPlayerWar/WarVC.swift
@@ -18,6 +18,8 @@ class WarVC: UIViewController {
     @IBOutlet weak var player2Area: PlayerArea!
     @IBOutlet weak var playedCard1Label: UILabel!
     @IBOutlet weak var playedCard2Label: UILabel!
+    @IBOutlet weak var play1Button: UIButton!
+    @IBOutlet weak var play2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +27,13 @@ class WarVC: UIViewController {
         // Do any additional setup after loading the view.
         //print("did load?")
         
-        player2Area.playerNameLabel.text = "Player 2"
         player1Area.cardsLeftLabel.text = "26"
+        
+        player2Area.playerNameLabel.text = "Player 2"
         player2Area.cardsLeftLabel.text = "26"
         player2Area.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        playedCard2Label.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        play2Button.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         
 //        player2Label.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
 //        player3Label.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
