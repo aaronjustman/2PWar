cba4bf3e4e16384b27f8eaf41c3d2ee59aca6f20
Aaron Justman <aaronjustman@divided-sky.home>
2021-05-16 11:56:10 -0700

    The minimum value for the war cards slider is 3.

--- TwoPlayerWar/WarVC.swift
+++ TwoPlayerWar/WarVC.swift
@@ -92,10 +92,10 @@ class WarVC: UIViewController, PlayButtonDelegate, OptionsDelegate, GADBannerVie
         p2CardIV.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         p2CardIV.image = .none
         
-        adBanner.adUnitID = "ca-app-pub-3940256099942544/2934735716"    // TEST ADS!!! Replace with actual id
-        adBanner.rootViewController = self
-        adBanner.delegate = self
-        adBanner.load(GADRequest())
+        //adBanner.adUnitID = "ca-app-pub-3940256099942544/2934735716"    // TEST ADS!!! Replace with actual id
+        //adBanner.rootViewController = self
+        //adBanner.delegate = self
+        //adBanner.load(GADRequest())
     }
     
     override func viewWillAppear(_ animated: Bool) {
